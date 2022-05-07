0ffba53da23c46973cdccef100bcc9c2b883056e
Oksana <kskaplun@gmail.com>
2022-05-07 12:22:06 +0300

    add 02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,24 +9,36 @@ class App extends Component {
   };
 
   addGood = () => {
-    this.setState(state => ({
-      good: state.good + 1,
+    this.setState(prevState => ({
+      good: prevState.good + 1,
     }));
   };
 
   addNeutral = () => {
-    this.setState(state => ({
-      neutral: state.neutral + 1,
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
     }));
   };
 
   addBad = () => {
-    this.setState(state => ({
-      bad: state.bad + 1,
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
     }));
   };
 
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    return Math.round((good / this.countTotalFeedback()) * 100) + '%';
+  };
+
   render() {
+    const { good, neutral, bad } = this.state;
+
     return (
       <div className={s.container}>
         <p>Please leave feedback</p>
@@ -35,41 +47,15 @@ class App extends Component {
         <button onClick={this.addBad}>Bad</button>
         <p>Statistics</p>
         <ul>
-          <li>Good: {this.state.good}</li>
-          <li>Neutral: {this.state.neutral}</li>
-          <li>Bad: {this.state.bad}</li>
+          <li>Good: {good}</li>
+          <li>Neutral: {neutral}</li>
+          <li>Bad: {bad}</li>
+          <li>Total: {this.countTotalFeedback()}</li>
+          <li>Positive feedback: {this.countPositiveFeedbackPercentage()}</li>
         </ul>
       </div>
     );
   }
 }
-
-// export const App = () => {
-//   return (
-//     <div
-//       className={s.container}
-//       style={{
-//         height: '100vh',
-//         // display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         textTransform: 'uppercase',
-//         color: '#010101',
-//       }}
-//     >
-//       <p>Please leave feedback</p>
-//       <button>Good</button>
-//       <button>Neutral</button>
-//       <button>Bad</button>
-//       <p>Statistics</p>
-//       <ul>
-//         <li>Good: </li>
-//         <li>Neutral: </li>
-//         <li>Bad: </li>
-//       </ul>
-//     </div>
-//   );
-// };
 
 export default App;
